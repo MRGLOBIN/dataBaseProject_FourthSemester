043a8f7e43bbff95880e8a9ca22c5f540fa42e6f
Muhammad Asim <asimqwe2@gmail.com>
2024-04-18 22:47:38 +0500

    add: html for navigation bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,38 @@
+import { Route, Routes } from 'react-router-dom'
+
 import logo from './logo.svg'
+import Home from './routes/home/home.components'
+import Auth from './routes/auth/auth.components'
+import Navigation from './routes/navigation/navigation.components'
+
 import './App.scss'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Navigation />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path='/auth' element={<Auth />}></Route>
+      </Routes>
+
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   )
 }
 
--- front-end/src/routes/navigation/navigation.components.jsx
+++ front-end/src/routes/navigation/navigation.components.jsx
@@ -1,7 +1,21 @@
+import { Link, Outlet } from 'react-router-dom'
+
 import './navigation.styles.scss'
 
 const Navigation = () => {
-  return <div></div>
+  return (
+    <>
+      <nav>
+        <Link to='/'>Logo</Link>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/contact-us'>Contact Us</Link>
+          <Link to='/auth'>Sign In</Link>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  )
 }
 
 export default Navigation
