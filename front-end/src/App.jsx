6443dbe7b39759389717fca9c5071d688d3d0185
Muhammad Asim <asimqwe2@gmail.com>
2024-04-24 21:31:11 +0500

    add: asset for Earth

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <header className='App-header'></header>
+        {/* <header className='App-header'></header> */}
         <EarthCanvas />
       </div>
     )
