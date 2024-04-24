7c64b9385d0d5eb8c949236b4657a6422ab2bd75
Muhammad Asim <asimqwe2@gmail.com>
2024-04-24 13:30:06 +0500

    fix: navigation component deleted from components and few code refactor

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,34 +1,35 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom'
 
-import logo from "./logo.svg";
-import Home from "./routes/home/home.components";
-import Auth from "./routes/auth/auth.components";
-import Navigation from "./routes/navigation/navigation.components";
+import logo from './logo.svg'
+import Home from './routes/home/home.components'
+import Auth from './routes/auth/auth.components'
+import Navigation from './routes/navigation/navigation.components'
 
-import "./App.scss";
-import React from "react";
-import Navigation from "./components/navigation/navigation.components";
-class App extends React {
+import './App.scss'
+import React from 'react'
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a 
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
+            {' '}
             Learn React
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
