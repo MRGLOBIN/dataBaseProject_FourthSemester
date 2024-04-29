a1369e988e1cf63c8ff6f1e4278361559b17a696
Muhammad Asim <asimqwe2@gmail.com>
2024-04-29 10:17:41 +0500

    ,

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-
+import { motion } from 'framer-motion'
 import logo from './logo.svg'
 import Home from './routes/home/home.components'
 import Auth from './routes/auth/auth.components'
@@ -8,8 +8,20 @@ import Navigation from './routes/navigation/navigation.components'
 import './App.scss'
 import React from 'react'
 
+import {
+  textVariant,
+  slideIn,
+  staggerContainer,
+  zoomIn,
+  fadeIn,
+} from './utils/framer-motion/motion'
+
 import EarthCanvas from './components/canvas/earth-canvas/earth-canvas.components'
 import StarsCanvas from './components/canvas/start-canvas/star-canvas.components'
+const variants = {
+  hidden: { x: '100%' },
+  visible: { x: 0 },
+}
 
 class App extends React.Component {
   render() {
@@ -17,10 +29,22 @@ class App extends React.Component {
       <div className='App'>
         <header className='App-header'></header>
         <div className='relative z-0 bg-black w-screen h-screen'>
-        <Navigation />
-        <EarthCanvas />
+          <Navigation />
 
-          <StarsCanvas />
+          <div
+            className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+          >
+            <motion.div
+              variants={zoomIn(0.2, 1)}
+              className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+              initial='hidden'
+              animate='show'
+            >
+              <h1 className='bg-white'>hello</h1>
+            </motion.div>
+          </div>
+          {/* <EarthCanvas /> */}
+          {/* <StarsCanvas /> */}
         </div>
       </div>
     )
--- front-end/src/routes/navigation/navigation.components.jsx
+++ front-end/src/routes/navigation/navigation.components.jsx
@@ -1,4 +1,3 @@
-
 import { Link, Outlet } from 'react-router-dom'
 
 import './navigation.styles.scss'
@@ -6,18 +5,25 @@ import './navigation.styles.scss'
 const Navigation = () => {
   return (
     <>
-    <header> 
-      <nav>
-        <Link to='/'>Logo</Link>
-      <ul class="nav_links">
-        <li><a href='#'>Project</a></li>
-        <li><a href='#'>Milestones</a></li>
-        <li><a href='#'>Deadlines</a></li>
-      <a  href='#'><button1> About</button1></a>
-        </ul>
-      </nav>
+      <header>
+        <nav>
+          <Link to='/'>Logo</Link>
+          <ul className='nav_links'>
+            <li>
+              <a href='#'>Project</a>
+            </li>
+            <li>
+              <a href='#'>Milestones</a>
+            </li>
+            <li>
+              <a href='#'>Deadlines</a>
+            </li>
+            <a href='#'>
+              <button> About</button>
+            </a>
+          </ul>
+        </nav>
       </header>
-      <Outlet />
     </>
   )
 }
